90929d7f8e3c922d2767373d43d2c549f6bbb160
yosef-tutian <yoseftutian@gmail.com>
2024-10-09 20:55:40 +0300

    use useDispatch to update data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import { Fragment } from "react";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />} <Counter />
+    </Fragment>
   );
 }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,8 +3,8 @@ import classes from "./Counter.module.css";
 import { counterActions } from "../store";
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
   const incrementHandler = () => {
     dispatch(counterActions.increment());
   };
